3fe7eb4feea50c907547b4df6cd6314884135d82
Qhama Hani <qhamahani@gmail.com>
2025-11-25 20:50:55 +0200

    Add Validation Logic

--- script.js
+++ script.js
@@ -9,19 +9,20 @@ document.getEventListener("DOMContentLoaded", function () {
   passwordInput = passwordInput.value.trim();
   const isValid = true;
   const messages = ["push"];
-  if (username.length < 3) {
-    isValid = false;
-    messages.push("Username must be at least 3 characters long.");
-  }
-  if (!email.includes("@") && !email.includes(".")) {
-    isValid = false;
-    messages.push("Please enter a valid email address.");
-  }
-  if (password.length < 8) {
-    isValid = false;
-    messages.push("Password must be at least 8 characters long.");
-  }
+
   form.addEventListener("submit", function (event) {
     event.preventDefault();
+    if (username.length < 3) {
+      isValid = false;
+      messages.push("Username must be at least 3 characters long.");
+    }
+    if (!email.includes("@") && !email.includes(".")) {
+      isValid = false;
+      messages.push("Please enter a valid email address.");
+    }
+    if (password.length < 8) {
+      isValid = false;
+      messages.push("Password must be at least 8 characters long.");
+    }
   });
 });
